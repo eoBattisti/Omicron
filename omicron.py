913af4f2399509bc47669ec9c5b7a09f0c35c663
Nicolas Battisti <nicolas.battisti@outlook.com>
2021-09-26 15:41:43 -0300

    update methods of listFiles and delete

--- omicron.py
+++ omicron.py
@@ -1,33 +1,38 @@
+from os import scandir
 import click
 import os.path
 import shutil
+
+from datetime import datetime
 from pathlib import Path
+
 
 def verify_dir(directory):
     if  os.path.isdir(directory):
         return True
     else:
+        click.secho("Please write a valid directory", fg='bright_red')
         return False
 
 def fileAtLocation(filename, path):
     if os.path.exists(path + "/" + filename):
         return True
-
+    else:
+        click.secho(f'{filename} is not in {path}, please enter a valid filename', fg='bright_red')
 # Verifica se o nome do arquivo realmente é um arquivo existe
 def verify_file(filename) -> bool:
     if os.path.isfile(filename):
         return True
     else:
+        click.secho("Please write a valid file", fg='bright_red')
         return False
 
 def isImage(file) -> bool:
     if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".svg") or file.endswith(".jpeg") or file.endswith(".gif")  or file.endswith(".ico"): 
-        
         return True 
     else:
         return False
-    
-        
+            
 def isPDF(file) -> bool:
     return True if file.endswith(".pdf") else False
 
@@ -42,6 +47,16 @@ def isPptOrPptx(file) -> bool:
 
 def isZipOrRar(file) -> bool:
     return True if file.endswith(".zip") or file.endswith(".rar") else False
+
+
+def convertDate(timestamp) -> str:
+    d = datetime.utcfromtimestamp(timestamp)
+    formated_date = d.strftime('%d %b %Y')
+    
+    return formated_date
+
+
+
 
 @click.group()
 def omicron():
@@ -71,15 +86,23 @@ def move_file(filename, fromn , to):
 
 
 @click.command(name='delete')
-@click.option('-f', '--filename', type=str, required=True)
+@click.option('--all/--one', default=False)
+@click.option('-f', '--filename',  type=str)
 @click.option('-d', '--directory', type=str, required=True)
-def delete_file(filename, directory):
+def delete_file(filename, directory, all):
     """Delete a file from a directory"""
-    # if the file is in the current directory, delete it
-    if fileAtLocation(filename, directory):
-        os.remove(directory + "/" + filename)
-        click.secho(f'{filename} deleted', fg='green')
-
+    # Delete all files from a directory
+    if all:
+        if click.confirm(click.style(f'This option wil delete all files from {directory}?', fg='bright_cyan'), abort=True):
+            for files in os.listdir(directory):
+                os.remove(directory + "/" + files)
+                click.secho(f'{files}: successfully deleted', fg='green')
+    else:
+        # if the file is in the current directory, delete it
+        filename = click.prompt(click.style("What file you wnat to remove", fg='bright_cyan'), type=str)
+        if fileAtLocation(filename, directory):
+            os.remove(directory + "/" + filename)
+            click.secho(f'{filename} deleted', fg='green')
 
 
 @click.command(name='listFiles')
@@ -87,11 +110,13 @@ def delete_file(filename, directory):
 def listFiles(directory):
     """ List all files from a directory"""
     # Verify if the directory exists
+    dir_files = scandir(directory)
     if verify_dir(directory):
-        for file in os.listdir(directory):
+        for file in dir_files:
             # Verify if the file exist in the current directory
-            if fileAtLocation(file, directory):
-                click.secho(f'{file}', fg='yellow')
+            if file.is_file():
+                info = file.stat()
+                click.secho(f'{file.name} \t\t Last Modified: {convertDate(info.st_mtime)} \t Size: {info.st_size} bytes', fg='bright_yellow')
 
 
 @click.command(name='organize')
